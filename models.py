6e6ec7f193410e9303ffca0983003d3878458b00
Chris Engelhart <engleman@gmail.com>
2025-07-18 13:54:44 -0400

    begin specifying DB schema using SQLModel

---
+++ models.py
@@ -0,0 +1,35 @@
+"""Signacula DB and API"""
+
+import datetime
+from sqlmodel import Field, SQLModel
+
+
+class Bookmark(SQLModel, table=True):
+    """A bookmarked URL"""
+    id: int | None = Field(default=None, primary_key=True)
+    url: str
+    title: str | None = None
+    backend: str | None = None
+    frequency: int | None = None
+    favicon: None = None  # TODO
+    description: str | None = None
+    image: None = None  # TODO
+    site_name: str | None = None
+    rating: float | None = None
+
+
+class Tag(SQLModel, table=True):
+    """A tag on a bookmark"""
+    id: int | None = Field(default=None, primary_key=True)
+    tag: str
+    bookmark_id: int = Field(foreign_key="bookmark.id")
+
+class Action(SQLModel, table=True):
+    """A record taken of an action on a bookmark"""
+    id: int | None = Field(default=None, primary_key=True)
+    bookmark_id: int = Field(foreign_key="bookmark.id")
+    action_type: None = None  # TODO archive, spider, review, develop
+    started: datetime.datetime | None = None
+    completed: datetime.datetime | None = None
+    error: str | None
+    result: str | None
